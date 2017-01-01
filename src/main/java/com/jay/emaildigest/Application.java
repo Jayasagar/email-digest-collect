c3c250c03f618ea99d735354e943d185057ee399
Jayasagar Jagirapu <jayasagar@gmail.com>
2017-01-01 11:24:38 +0100

    remove secret key and remove unnecessary class

--- src/main/java/com/jay/emaildigest/Application.java
+++ src/main/java/com/jay/emaildigest/Application.java
@@ -4,11 +4,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-//@ImportResource("classpath:aws-sns.xml")
 public class Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Application.class).web(true).run(args);
